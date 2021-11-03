bf7a465d7eab7694315a91ae7b9b2f87d57a63f2
mehfius <matheus.ferraz@gmail.com>
2021-11-03 15:38:08 +0000

    3

--- config/supa.js
+++ config/supa.js
@@ -1,13 +1,11 @@
 const { createClient } = require("@supabase/supabase-js");
 
 var connSupa = function () {
-  const url = "https://bhxafhsowweifsrnyfaa.supabase.co";
-  const key =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMzY0NzMxMCwiZXhwIjoxOTQ5MjIzMzEwfQ.vblH3ycs3KqRAOL2TT-XSQ4UWQ0uWAIzEE-_8cB4_6U";
 
   console.log("Conecta no supa");
 
-  return createClient(url, key);
+  return createClient(process.env['url'],process.env['anom_key'])
+
 };
  
 module.exports = function () {
--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 var app = require("./config/server");
 
-// var rotaNoticias=require('./app/routes/noticias')(app);
-
-// var rotaHome=require('./app/routes/home')(app);
-
 app.listen(3000, function () {
   console.log("rodando");
 });
